c85c25f4fd8475cf06be28d7f55f8db872ab17b1
Aurelie Gedeon <agedeon540@gmail.com>
2022-02-28 11:38:15 -0500

    built api for authentication practice

---
+++ src/users.js
@@ -0,0 +1,21 @@
+exports.createUser = (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send("invalid request");
+    // return;
+  }
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    isAdmin: false,
+    userRate: 5,
+  };
+
+  //   const db = connectDb();
+  //   db.collection("user")
+  //     .add(newUser)
+  //     .then((doc) => {
+  //TODO: create a JWT and end back to token
+  res.status(201).send("Account created");
+  // })
+  // .catch((err) => res.status(500).send(err));
+};
