ffaa21c2c5fd1704fcbe4510cdbb762ee4a759e7
Aurelie Gedeon <agedeon540@gmail.com>
2022-03-01 09:21:40 -0500

    added login function to api

--- src/users.js
+++ src/users.js
@@ -3,9 +3,10 @@ const { connectDb } = require("./dbConnect");
 exports.createUser = (req, res) => {
   if (!req.body || !req.body.email || !req.body.password) {
     res.status(400).send("invalid request");
+    return;
   }
   const newUser = {
-    email: req.body.email,
+    email: req.body.email.toLowerCase(),
     password: req.body.password,
     isAdmin: false,
     userRate: 5,
@@ -15,8 +16,45 @@ exports.createUser = (req, res) => {
   db.collection("user")
     .add(newUser)
     .then((doc) => {
+      const user = {
+        id: doc.id,
+        email: newUser.email,
+        isAdmin: false,
+        userRole: 5,
+      };
       //TODO: create a JWT and end back to token
-      res.status(201).send("Account created");
+      res.status(201).send({
+        success: true,
+        message: "Account created",
+        token: user,
+      });
     })
-    .catch((err) => res.status(500).send(err));
+    .catch((err) =>
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      })
+    );
 };
+
+exports.loginUser = (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send("invalid request");
+    return;
+  }
+};
+
+const db = connectDb();
+db.collection("users")
+  .where("email", "==", req.body.email.toLowerCase())
+  .where("password", "==", req.body.password)
+  .get()
+  .then()
+  .catch((err) =>
+    res.status(500).send({
+      success: false,
+      message: err.message,
+      error: err,
+    })
+  );
